d5b094b5609af3bfd316e938d116935897bffb9c
Jane Roxanne <janeroxanne0@gmail.com>
2019-10-20 15:03:03 -0500

    Indentation fix.

--- src/main/kotlin/net/shadowkat/minecraft/opensolidstate/OssmEvents.kt
+++ src/main/kotlin/net/shadowkat/minecraft/opensolidstate/OssmEvents.kt
@@ -10,22 +10,22 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod.EventBusSubscriber(modid = "ossm")
 object OssmEvents {
-    lateinit var shit: Array<Item>
+	lateinit var shit: Array<Item>
 
-    fun init() {
-        shit = arrayOf(OssmEeprom(0, false), OssmEeprom(0, true), OssmEeprom(1, false), OssmEeprom(1, true), OssmEeprom(2, false), OssmEeprom(2, true), OssmEeprom(3, false), OssmEeprom(3, true))
-    }
+	fun init() {
+		shit = arrayOf(OssmEeprom(0, false), OssmEeprom(0, true), OssmEeprom(1, false), OssmEeprom(1, true), OssmEeprom(2, false), OssmEeprom(2, true), OssmEeprom(3, false), OssmEeprom(3, true))
+	}
 
-    @SubscribeEvent
-    fun registerItems(event: RegistryEvent.Register<Item>) {
-        for (i in shit.indices)
-            event.registry.register(shit[i])
-    }
+	@SubscribeEvent
+	fun registerItems(event: RegistryEvent.Register<Item>) {
+		for (i in shit.indices)
+			event.registry.register(shit[i])
+	}
 
-    @SubscribeEvent
-    fun registerRenders(event: ModelRegistryEvent) {
-        for (i in shit.indices)
-            ModelLoader.setCustomModelResourceLocation(shit[i], 0, ModelResourceLocation(shit[i].registryName!!, "inventory"))
-        //ModelLoader.set
-    }
+	@SubscribeEvent
+	fun registerRenders(event: ModelRegistryEvent) {
+		for (i in shit.indices)
+			ModelLoader.setCustomModelResourceLocation(shit[i], 0, ModelResourceLocation(shit[i].registryName!!, "inventory"))
+		//ModelLoader.set
+	}
 }
