b417d0debff50171a2aec421dfcc7936122a44ad
Jane Roxanne <janeroxanne0@gmail.com>
2019-10-19 20:31:29 -0500

    IDEA defaulted to spaces, for some reason. Tabs superior.

--- src/main/kotlin/net/shadowkat/minecraft/opensolidstate/OssmEeprom.kt
+++ src/main/kotlin/net/shadowkat/minecraft/opensolidstate/OssmEeprom.kt
@@ -4,29 +4,29 @@ import li.cil.oc.api.CreativeTab
 import net.minecraft.item.Item
 
 class OssmEeprom : Item {
-    private val tier : Int
-    private val card : Boolean
+	private val tier : Int
+	private val card : Boolean
 
-    constructor(tier : Int, card : Boolean) {
-        this.tier = tier
-        this.card = card
-        creativeTab = CreativeTab.instance;
-        unlocalizedName = "ossm_prom_${tier}${cardText()}"
-        setRegistryName("ossm:prom_${tier}${cardText()}")
-    }
+	constructor(tier : Int, card : Boolean) {
+		this.tier = tier
+		this.card = card
+		creativeTab = CreativeTab.instance;
+		unlocalizedName = "ossm_prom_${tier}${cardText()}"
+		setRegistryName("ossm:prom_${tier}${cardText()}")
+	}
 
-    private fun cardText() : String {
-        if (card)
-            return "_card"
-        else
-            return ""
-    }
+	private fun cardText() : String {
+		if (card)
+			return "_card"
+		else
+			return ""
+	}
 
-    fun getTier() : Int {
-        return tier
-    }
+	fun getTier() : Int {
+		return tier
+	}
 
-    fun getCard() : Boolean {
-        return card
-    }
+	fun getCard() : Boolean {
+		return card
+	}
 }
